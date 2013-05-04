106e0b95542a175048a95adddb6d044d42863b9c
Jamison Dance <jergason@gmail.com>
2013-05-03 20:48:31 -0600

    add server

---
+++ index.js
@@ -0,0 +1,18 @@
+var path = require('path')
+var express = require('express')
+
+var app = express()
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.bodyParser())
+
+app.get('/prompt', function(req, res) {
+  var prompts = ['what', 'who', 'why', 'when', 'durp', 'hurp', 'foo']
+  var randIndex = Math.floor(Math.random() * prompts.length)
+
+  res.json({ prompt: promts[randIndex] })
+})
+
+
+console.log('started yo')
+app.listen(1700)
